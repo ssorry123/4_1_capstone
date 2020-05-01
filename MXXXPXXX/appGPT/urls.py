a6695dd3ff20290782ef1ac30ef1aa188ad4d056
서원 <26060283@naver.com>
2020-05-01 14:35:46 +0000

    구축테스트

---
+++ MXXXPXXX/appGPT/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from . import views #.은 현재폴더의 디렉토리라는뜻. 즉 현재폴더의 views.py를 import하는것임
+
+urlpatterns = [
+    path('', views.index),
+]
--- minepost/Gpt/KOGPTXXX/down.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-import random
-import torch
-from gluonnlp.data import SentencepieceTokenizer 
-sys.path.append('/content/drive/My Drive/')
-from NarrativeKoGPT2.kogpt2.utils import get_tokenizer
