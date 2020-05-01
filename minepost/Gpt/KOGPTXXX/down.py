e9d0c363110d1fbba4528f29daa8bef136a0150f
서원 <26060283@naver.com>
2020-05-01 12:23:22 +0000

    구축테스트1

---
+++ minepost/Gpt/KOGPTXXX/down.py
@@ -0,0 +1,7 @@
+import os
+import sys
+import random
+import torch
+from gluonnlp.data import SentencepieceTokenizer 
+sys.path.append('/content/drive/My Drive/')
+from NarrativeKoGPT2.kogpt2.utils import get_tokenizer
