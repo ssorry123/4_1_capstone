93da80d01de5fba3e256c6a1e8b72faabe565029
gf234 <gf265@naver.com>
2020-06-01 21:51:45 +0900

    update

--- MXXXPXXX/sg/models.py
+++ MXXXPXXX/sg/models.py
@@ -22,7 +22,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['name', 'email']
 
     def __str__(self):
-        return self.userid
+        return self.name
 
 
 class Writing(models.Model):
