f588075ab9ba96bd4a368ba0b55cd2cf07acab01
서원 <26060283@naver.com>
2020-06-01 18:52:46 +0000

    석진형파일추가..

--- MXXXPXXX/sg/static/sg/script.js
+++ MXXXPXXX/sg/static/sg/script.js
@@ -13,3 +13,36 @@ function setThumbnail(event) {
   
   reader.readAsDataURL(event.target.files[0]); 
 }
+
+//제목 단어 추천
+
+function recommendTitle(val){
+
+  var res1 = val+"추천함";
+  var res2 = val+"ㄱㄱ";
+  var res3 = val+"ㅎㅇ";
+  var res4 = val+"어때";
+  var res5 = val+"써";
+  
+  if(val == ""){
+      document.getElementById('result').value = "0";
+  } else {
+      document.getElementById('result1').value = res1;
+      document.getElementById('result2').value = res2;
+      document.getElementById('result3').value = res3;
+      document.getElementById('result4').value = res4;
+      document.getElementById('result5').value = res5;
+  }
+}
+
+function addTitle(val) {
+  var new_title = document.getElementById('title').value += val
+  document.getElementById('title').value = new_title;
+  recommendTitle(new_title);
+}
+
+//이미지 클릭시 자동 넣기
+
+function mark(img) {
+  img.style.border = "1px solid blue";
+}
